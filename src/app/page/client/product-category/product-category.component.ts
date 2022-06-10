e5a2c67934c57dd910a6b5b2614114e2c3ab2014
Lê Thanh Bình <lethanhbinh26062002@gmail.com>
2022-06-10 11:20:00 +0700

    Asm_1

---
+++ src/app/page/client/product-category/product-category.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
+import { Category } from 'src/app/type/Category';
+import { Product } from 'src/app/type/Product';
+
+@Component({
+  selector: 'app-product-category',
+  templateUrl: './product-category.component.html',
+  styleUrls: ['./product-category.component.css']
+})
+export class ProductCategoryComponent implements OnInit {
+  products: Product[];
+  constructor(
+    private productService: ProductService,
+    private activateRoute: ActivatedRoute
+  ) { 
+    this.products = [];
+  }
+  ngOnInit(): void {
+    this.onGetList();
+  }
+  onGetList() {
+    const idFromUrl = this.activateRoute.snapshot.params['id'];
+    this.productService.getProductsByCategory(idFromUrl).subscribe((data) => {
+      this.products = data;
+      console.log(data);
+      
+    console.log(JSON.stringify(data,null , ' '));
+
+    });
+  }
+}
